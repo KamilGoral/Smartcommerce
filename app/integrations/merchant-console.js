e677a2e04f53f8b35d0b2682a76976142064788b
KamilGoral <kamil.goral@smartcommerce.net>
2023-01-08 13:46:42 +0100

    path merchant-console

--- app/integrations/merchant-console.js
+++ app/integrations/merchant-console.js
@@ -179,10 +179,10 @@ docReady(function() {
           }
         ],
         rowCallback: function (row, data) {
-            console.log(row);
             console.log(data.merchantConsoleShopId);
             console.log($("td:eq(3) select", row));
-            $("td:eq(3) select", row).removeAttr('selected').filter('[value=' + data.merchantConsoleShopId +']').attr('selected', true)
+            $("td:eq(3) select", row).val(data.merchantConsoleShopId);
+            $("td:eq(3) select", row).change();
           }
       });
     };
