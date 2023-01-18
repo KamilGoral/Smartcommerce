26d7a046825fbaedb10221848ca968af82bf2b06
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2023-01-18 07:41:46 +0100

    fix pc market into merchant-console

--- app/integrations/merchant-console.js
+++ app/integrations/merchant-console.js
@@ -13,107 +13,123 @@ function docReady(fn) {
   }
 }
 
-docReady(function() {
+docReady(function () {
   function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-  };
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
   var orgToken = getCookie("sprytnyToken");
   var InvokeURL = getCookie("sprytnyInvokeURL");
   var DomainName = getCookie("sprytnyDomainName");
   var shopKey = new URL(location.href).searchParams.get("shopKey");
   var offerId = new URL(location.href).searchParams.get("offerId");
-  var integrationKeyId = "merchant-console"
+  var integrationKeyId = "merchant-console";
   var smartToken = getCookie("sprytnycookie");
-  document.getElementById('waitingdots').style.display = "flex";
-  document.getElementById('Sample-Integration').style.display = "none";
-  var ClientID = sessionStorage.getItem('OrganizationclientId')
-  var OrganizationName = sessionStorage.getItem('OrganizationName')
+  document.getElementById("waitingdots").style.display = "flex";
+  document.getElementById("Sample-Integration").style.display = "none";
+  var ClientID = sessionStorage.getItem("OrganizationclientId");
+  var OrganizationName = sessionStorage.getItem("OrganizationName");
   const IntegrationBread = document.getElementById("IntegrationBread");
   IntegrationBread.setAttribute("href", window.location.href);
   const OrganizationBread0 = document.getElementById("OrganizationBread0");
   OrganizationBread0.textContent = OrganizationName;
-  OrganizationBread0.setAttribute("href", "https://" + DomainName + "/app/tenants/organization?name=" + OrganizationName + "&clientId=" + ClientID);
+  OrganizationBread0.setAttribute(
+    "href",
+    "https://" +
+      DomainName +
+      "/app/tenants/organization?name=" +
+      OrganizationName +
+      "&clientId=" +
+      ClientID
+  );
 
   function getIntegrations() {
-    let url = new URL(InvokeURL + 'integrations/merchant-console');
+    let url = new URL(InvokeURL + "integrations/merchant-console");
     let request = new XMLHttpRequest();
-    request.open('GET', url, true);
+    request.open("GET", url, true);
     request.setRequestHeader("Authorization", orgToken);
-    request.onload = function() {
+    request.onload = function () {
       var data = JSON.parse(this.response);
       if (request.status >= 200 && request.status < 400) {
-        document.getElementById('Sample-Integration').style.display = "grid";
+        document.getElementById("Sample-Integration").style.display = "grid";
 
-        const integrationDescription = document.getElementById('integrationDescription');
+        const integrationDescription = document.getElementById(
+          "integrationDescription"
+        );
         integrationDescription.textContent = data.description;
-        const integrationLogo = document.getElementById('integrationLogo');
-        integrationLogo.src = 'data:image/png;base64,' + data.image;
-        const integrationStatus = document.getElementById('integrationStatus');
-        const integrationButton = document.getElementById('integrationButton');
+        const integrationLogo = document.getElementById("integrationLogo");
+        integrationLogo.src = "data:image/png;base64," + data.image;
+        const integrationStatus = document.getElementById("integrationStatus");
+        const integrationButton = document.getElementById("integrationButton");
 
         if (data.enabled === true) {
           integrationStatus.textContent = "Aktywny";
-          integrationStatus.style.color = 'green';
+          integrationStatus.style.color = "green";
           integrationButton.value = "Zmień dane logowania";
         } else {
           integrationStatus.textContent = "Nieaktywny";
-        };
-        const integrationLogin = document.getElementById('Username');
+        }
+        const integrationLogin = document.getElementById("Username");
         integrationLogin.value = data.credentials.username;
-        const integrationHost = document.getElementById('Host');
+        const integrationHost = document.getElementById("Host");
         integrationHost.value = data.credentials.host;
-        const integrationPort = document.getElementById('Port');
+        const integrationPort = document.getElementById("Port");
         integrationPort.value = data.credentials.port;
-        const integrationEngine = document.getElementById('engine');
+        const integrationEngine = document.getElementById("engine");
         integrationEngine.value = data.credentials.engine;
-        const integrationDbName = document.getElementById('dbname');
+        const integrationDbName = document.getElementById("dbname");
         integrationDbName.value = data.credentials.dbname;
         if (request.status === 401) {
           console.log("Unauthorized");
-        };
-      };
+        }
+      }
     };
     request.send();
-  };
+  }
 
   function getShops() {
-
     async function getIDS() {
-      let times = [""]
-      let url2 = new URL(InvokeURL + 'integrations/merchant-console/shops');
+      let times = [""];
+      let url2 = new URL(InvokeURL + "integrations/merchant-console/shops");
       let request2 = new XMLHttpRequest();
-      request2.open('GET', url2, true);
+      request2.open("GET", url2, true);
       request2.setRequestHeader("Authorization", orgToken);
-      request2.onload = async function() {
+      request2.onload = async function () {
         var data2 = JSON.parse(this.response);
         var toParse2 = data2.items;
         if (request2.status >= 200 && request2.status < 400) {
           toParse2.forEach((item) => {
             times.push(item.id);
           });
-          createAll(times)
-          return await new Promise(resolve => request2.onload = resolve(times));
-        };
+          createAll(times);
+          return await new Promise(
+            (resolve) => (request2.onload = resolve(times))
+          );
+        }
         if (request2.status == 401) {
           console.log("Unauthorized");
-          console.log(times)
-          return await new Promise(resolve => request2.onload = resolve(times));
+          console.log(times);
+          return await new Promise(
+            (resolve) => (request2.onload = resolve(times))
+          );
         }
-      }
+      };
       request2.send();
     }
 
     function createAll(sklepy) {
-      const optionsHTML = sklepy.reduce((html, value) => html + `<option value="${value}">${value}</option>`, "");
+      const optionsHTML = sklepy.reduce(
+        (html, value) => html + `<option value="${value}">${value}</option>`,
+        ""
+      );
       const selectHTML = `<select class="id100">${optionsHTML}</select>`;
 
       let url = new URL(InvokeURL + "shops");
       let request = new XMLHttpRequest();
       request.open("GET", url, true);
       request.setRequestHeader("Authorization", orgToken);
-      request.onload = function() {
+      request.onload = function () {
         var data2 = JSON.parse(this.response);
         var table = $("#table_integrated_shops_list").DataTable({
           data: data2.items,
@@ -144,16 +160,18 @@ docReady(function() {
               sortDescending: ": Sortowanie malejące",
             },
           },
-          columns: [{
+          columns: [
+            {
               orderable: false,
               data: null,
               width: "36px",
-              defaultContent: "<div class='details-container2'><img src='https://uploads-ssl.webflow.com/6041108bece36760b4e14016/61ae41350933c525ec8ea03a_office-building.svg' alt='offer'></img></div>",
+              defaultContent:
+                "<div class='details-container2'><img src='https://uploads-ssl.webflow.com/6041108bece36760b4e14016/61ae41350933c525ec8ea03a_office-building.svg' alt='offer'></img></div>",
             },
             {
               orderable: true,
               data: "name",
-              render: function(data) {
+              render: function (data) {
                 if (data !== null) {
                   return data;
                 }
@@ -165,7 +183,7 @@ docReady(function() {
             {
               orderable: true,
               data: "shopKey",
-              render: function(data) {
+              render: function (data) {
                 if (data !== null) {
                   return data;
                 }
@@ -177,68 +195,75 @@ docReady(function() {
             {
               orderable: true,
               data: "merchantConsoleShopId",
-              "render": function(data) {
-                return selectHTML.toString()
-              }
-            }
+              render: function (data) {
+                return selectHTML.toString();
+              },
+            },
           ],
-          rowCallback: function(row, data) {
+          rowCallback: function (row, data) {
             $("td:eq(3) select", row).val(data.merchantConsoleShopId);
             $("td:eq(3) select", row).change();
-          }
+          },
         });
-        $(".id100").on("change",function() {
+        $(".id100").on("change", function () {
           var merchantConsoleShopId = parseInt($(this).val());
-          var row = $(this).closest('tr');
+          var row = $(this).closest("tr");
           var shopKey = table.row(row).data().shopKey;
           var previousMCSId = table.row(row).data().merchantConsoleShopId;
           var columnData = table.column(3).data();
-          console.log("Prveious:" + columnData)
+          console.log("Prveious:" + columnData);
           console.log("Prveious: " + previousMCSId);
           console.log("sklepy: " + sklepy);
           console.log("mcshopId: " + merchantConsoleShopId);
 
           var payload = [];
           var method = "PATCH";
-          
 
           if (isNaN(merchantConsoleShopId)) {
-            var action = InvokeURL + "integrations/merchant-console/shops/" + previousMCSId;
-              console.log("delete")
-              var content = {
-                op: "remove",
-                path: "/shopKey",
-              };
-              payload.push(content);
+            var action =
+              InvokeURL +
+              "integrations/merchant-console/shops/" +
+              previousMCSId;
+            console.log("delete");
+            var content = {
+              op: "remove",
+              path: "/shopKey",
+            };
+            payload.push(content);
           } else if (previousMCSId > 0) {
-            console.log("replace")
-            var action = InvokeURL + "integrations/merchant-console/shops/" + merchantConsoleShopId;
+            console.log("replace");
+            var action =
+              InvokeURL +
+              "integrations/merchant-console/shops/" +
+              merchantConsoleShopId;
             var content = {
               op: "replace",
               path: "/shopKey",
-              value: shopKey
-            }
+              value: shopKey,
+            };
             payload.push(content);
           } else {
-            console.log("add")
-            var action = InvokeURL + "integrations/merchant-console/shops/" + merchantConsoleShopId;
+            console.log("add");
+            var action =
+              InvokeURL +
+              "integrations/merchant-console/shops/" +
+              merchantConsoleShopId;
             var content = {
               op: "add",
               path: "/shopKey",
-              value: shopKey
-            }
+              value: shopKey,
+            };
             payload.push(content);
           }
-        
 
           $.ajax({
             type: method,
             url: action,
             cors: true,
-            beforeSend: function() {
+            beforeSend: function () {
               $("#waitingdots").show();
             },
-            complete: function() {
+            complete: function () {
               $("#waitingdots").hide();
             },
             contentType: "application/json",
@@ -250,15 +275,19 @@ docReady(function() {
             },
             data: JSON.stringify(payload),
             processData: false,
-            success: function(resultData) {
+            success: function (resultData) {
               console.log("success");
               console.log(resultData);
               $(".warningmessagetext").css("color", "#ffffff");
-              if (isNaN(merchantConsoleShopId)) {               
-                $(".warningmessagetext").text("Sukces. Pomyślnie usunięto integracje sklepu z Konsolą Kupca");
+              if (isNaN(merchantConsoleShopId)) {
+                $(".warningmessagetext").text(
+                  "Sukces. Pomyślnie usunięto integracje sklepu z Konsolą Kupca"
+                );
               } else {
-                $(".warningmessagetext").text("Sukces. Pomyślnie zintegrowano sklep z Konsolą Kupca");
-              } 
+                $(".warningmessagetext").text(
+                  "Sukces. Pomyślnie zintegrowano sklep z Konsolą Kupca"
+                );
+              }
               $(".error-message-fixed-main").css("background-color", "#00754e");
               $("#WarningMessageContainer").show();
               $("#WarningMessageContainer").fadeOut(6000);
@@ -269,10 +298,9 @@ docReady(function() {
                   return;
                 }
               }
-
             },
-            error: function(jqXHR, exception) {
-              console.log("error")
+            error: function (jqXHR, exception) {
+              console.log("error");
               console.log(jqXHR);
               console.log(exception);
               var msg = "";
@@ -297,7 +325,7 @@ docReady(function() {
               } else {
                 msg = "" + jqXHR.responseText;
               }
-                
+
               $(".warningmessagetext").css("color", "#3a4570");
               $(".warningmessagetext").text(msg);
               $(".error-message-fixed-main").css("background-color", "#ffc53d");
@@ -312,51 +340,49 @@ docReady(function() {
     }
 
     async function main() {
-        let result = await getIDS();
+      let result = await getIDS();
     }
     main();
-
   }
 
-  makeWebflowFormAjaxCreate = function(forms, successCallback, errorCallback) {
-    forms.each(function() {
+  makeWebflowFormAjaxCreate = function (forms, successCallback, errorCallback) {
+    forms.each(function () {
       var form = $(this);
-      form.on("submit", function(event) {
+      form.on("submit", function (event) {
         var container = form.parent();
         var doneBlock = $(".w-form-done", container);
         var failBlock = $(".w-form-fail", container);
         var inputdata = form.serializeArray();
 
         var data = {
-          'username': inputdata[0].value,
-          'password': inputdata[1].value,
-          'host': inputdata[2].value,
-          'port': parseInt(inputdata[3].value),
-          'engine': inputdata[4].value,
-          'dbname': inputdata[5].value
+          username: inputdata[0].value,
+          password: inputdata[1].value,
+          host: inputdata[2].value,
+          port: parseInt(inputdata[3].value),
+          engine: inputdata[4].value,
+          dbname: inputdata[5].value,
         };
-
 
         $.ajax({
           type: "PUT",
-          url: InvokeURL + "integrations/pcmarket",
+          url: InvokeURL + "integrations/merchant-console",
           cors: true,
-          beforeSend: function() {
-            $('#waitingdots').show();
+          beforeSend: function () {
+            $("#waitingdots").show();
           },
-          complete: function() {
-            $('#waitingdots').hide();
+          complete: function () {
+            $("#waitingdots").hide();
           },
-          contentType: 'application/json',
-          dataType: 'json',
+          contentType: "application/json",
+          dataType: "json",
           headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Authorization': orgToken
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: orgToken,
           },
           data: JSON.stringify(data),
-          success: function(resultData) {
-            if (typeof successCallback === 'function') {
+          success: function (resultData) {
+            if (typeof successCallback === "function") {
               result = successCallback(resultData);
               if (!result) {
                 form.show();
@@ -369,19 +395,19 @@ docReady(function() {
             form.hide();
             doneBlock.show();
             failBlock.hide();
-            window.setTimeout(function() {
+            window.setTimeout(function () {
               location.reload();
             }, 1000);
           },
-          error: function(e) {
-            if (typeof errorCallback === 'function') {
-              errorCallback(e)
+          error: function (e) {
+            if (typeof errorCallback === "function") {
+              errorCallback(e);
             }
             form.show();
             doneBlock.hide();
             failBlock.show();
             console.log(e);
-          }
+          },
         });
         event.preventDefault();
         return false;
@@ -431,7 +457,13 @@ docReady(function() {
             doneBlock.show();
             failBlock.hide();
             window.setTimeout(function () {
-              document.location = "href", "https://" + DomainName + "/app/tenants/organization?name=" + OrganizationName + "&clientId=" + ClientID;
+              (document.location = "href"),
+                "https://" +
+                  DomainName +
+                  "/app/tenants/organization?name=" +
+                  OrganizationName +
+                  "&clientId=" +
+                  ClientID;
             }, 5000);
           },
           error: function (e) {
@@ -452,8 +484,7 @@ docReady(function() {
 
   getIntegrations();
   getShops();
-  $('#waitingdots').hide();
+  $("#waitingdots").hide();
   makeWebflowFormAjaxCreate($("#wf-form-pcmarket"));
   makeWebflowFormAjaxDelete($("#wf-form-DeleteIntegration"));
-
-})
+});
