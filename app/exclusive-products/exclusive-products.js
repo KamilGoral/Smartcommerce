65e426264feae33668a1f115adf11acc3e77147a
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-01-26 17:52:00 +0100

    fix single exclusive

--- app/exclusive-products/exclusive-products.js
+++ app/exclusive-products/exclusive-products.js
@@ -637,7 +637,7 @@ docReady(function () {
           var nowDate = new Date();
           nowDate.setUTCHours(0, 0, 0, 0);
 
-          if (endDate <= nowDate) {
+          if (endDate >= nowDate) {
             return "<img style='cursor: pointer' src='https://uploads-ssl.webflow.com/6041108bece36760b4e14016/640442ed27be9b5e30c7dc31_edit.svg' action='edit' alt='edit'></img><img style='cursor: pointer' src='https://uploads-ssl.webflow.com/6041108bece36760b4e14016/6404b6547ad4e00f24ccb7f6_trash.svg' action='delete' alt='delete'></img>";
           } else {
             return "<img style='opacity:0.7;cursor: not-allowed !important' src='https://uploads-ssl.webflow.com/6041108bece36760b4e14016/640442ed27be9b5e30c7dc31_edit.svg' action='disabled' alt='disabled'></img><img style='opacity:0.7;cursor: not-allowed !important' src='https://uploads-ssl.webflow.com/6041108bece36760b4e14016/6404b6547ad4e00f24ccb7f6_trash.svg' action='disabled' alt='disabled'></img>";
