372f50e74002e1f2ebd78580660de8753fb01e49
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-04-27 10:33:37 +0200

    onboarding

--- app/utils/shop-onboarding.js
+++ app/utils/shop-onboarding.js
@@ -22,34 +22,33 @@ docReady(function () {
 
   function getCookieNameByValue(searchValue) {
     // Get all cookies as a single string and split it into individual cookies
-    const cookies = document.cookie.split('; ');
-    
+    const cookies = document.cookie.split("; ");
+
     // Iterate through each cookie string
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i];
-      const [name, value] = cookie.split('=');  // Split each cookie into name and value
-  
+      const [name, value] = cookie.split("="); // Split each cookie into name and value
+
       // Decode the cookie value and compare it to the searchValue
       if (decodeURIComponent(value) === searchValue) {
-        return name;  // Return the cookie name if the values match
+        return name; // Return the cookie name if the values match
       }
     }
-  
+
     return null; // Return null if no matching value is found
   }
 
   // Find all buttons with the class 'nextstep'
-  const nextStepButtons = document.querySelectorAll('.nextstep');
-    
+  const nextStepButtons = document.querySelectorAll(".nextstep");
+
   // Add a click event listener to each 'nextstep' button
-  nextStepButtons.forEach(button => {
-    button.addEventListener('click', function() {
+  nextStepButtons.forEach((button) => {
+    button.addEventListener("click", function () {
       // Find the button with the ID 'forwardButton' and simulate a click
-      const forwardButton = document.getElementById('forwardButton');
+      const forwardButton = document.getElementById("forwardButton");
       if (forwardButton) {
         forwardButton.click();
       }
     });
   });
-
 });
