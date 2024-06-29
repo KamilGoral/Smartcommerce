ed4339ed38af22abd1080ac80a521bc126b9a010
KamilGoral <kamil.goral@smartcommerce.net>
2024-06-29 18:50:27 +0200

    new me

--- app/users/me-old.js
+++ app/users/me-old.js
@@ -484,11 +484,23 @@ docReady(function () {
 
             // If the organization is onboarding, remove click actions and add class .tippy
             if (organization.status.toLowerCase() === "onboarding") {
-              
+
               createOrgButton.style.pointerEvents = "none"; // Disable pointer events
               createOrgButton.classList.add("tippy"); // Add the tippy class
               createOrgButton.style.opacity = "0.5"; // Set opacity to 50%
               createOrgButton.style.display = "flex";
+              tippy(createOrgButton, {
+                // Add the class tippy to your element
+                theme: "light", // Dark or Light
+                animation: "scale", // Options, shift-away, shift-toward, scale, persepctive
+                duration: 250, // Duration of the Animation
+                arrow: true, // Add arrow to the tooltip
+                arrowType: "round", // Sharp, round or empty for none
+                delay: [0, 50], // Trigger delay in & out
+                maxWidth: 240, // Optional, max width settings
+              });
+
+
             } else {
               row.addEventListener("click", LoginIntoOrganization, false);
             }
@@ -757,8 +769,6 @@ docReady(function () {
   getInvitations();
   getOrganizations();
   getUser();
-  
-  setTimeout(function() {
-    LoadTippy();
-}, 3000);
+  LoadTippy();
+
 });
