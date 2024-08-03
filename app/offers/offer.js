bb68e7f5bebe7a8806773e181f4199208e0855c4
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-08-03 11:50:53 +0200

    loading dots pricelist

--- app/offers/offer.js
+++ app/offers/offer.js
@@ -1571,6 +1571,7 @@ docReady(function () {
     const popupContent = document.getElementById("popupContent");
     var input = dataToDisplay.attr("data-content");
     var values = input.split(",");
+    var output = "";
 
     for (var i = 0; i < values.length; i++) {
       output += "<p>" + values[i] + "</p>";
