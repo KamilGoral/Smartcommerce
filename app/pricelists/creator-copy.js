27ed8188c2c79adc36dcc8af3e9ac0e3a5bf5565
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-08-03 08:43:35 +0200

    custom headers

---
+++ app/pricelists/creator-copy.js
@@ -0,0 +1,425 @@
+console.log("Script Loaded v3");
+function docReady(fn) {
+  // see if DOM is already available
+  if (
+    document.readyState === "complete" ||
+    document.readyState === "interactive"
+  ) {
+    // call on next available tick1
+    setTimeout(fn, 1);
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+docReady(function () {
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2)
+      return decodeURIComponent(parts.pop().split(";").shift());
+  }
+  // DOM is loaded and ready for manipulation here
+  const displayMessage = (type, message) => {
+    $("#Message-Container").show().delay(5000).fadeOut("slow");
+    if (message) {
+      $(`#${type}-Message-Text`).text(message);
+    }
+    $(`#${type}-Message`).show().delay(5000).fadeOut("slow");
+  };
+
+  var ecEnabledValue = getCookie("EcEnabled");
+  if (ecEnabledValue === "true") {
+    $("#alertMessage").show();
+  }
+
+  function setCookie(cName, cValue, expirationSec) {
+    let date = new Date();
+    date.setTime(date.getTime() + expirationSec * 1000);
+    const expires = "expires=" + date.toUTCString();
+    const encodedValue = encodeURIComponent(cValue);
+    document.cookie = `${cName}=${encodedValue}; ${expires}; path=/`;
+  }
+
+  function parseAttributes(cookieValue) {
+    const decodedValue = decodeURIComponent(cookieValue);
+    const attributes = decodedValue.split("|");
+    const result = {};
+    attributes.forEach((attribute) => {
+      const [key, value] = attribute.split(":");
+      result[key.trim()] = value.trim();
+    });
+    return result;
+  }
+
+  var smartToken = getCookie("sprytnycookie");
+  var accessToken = smartToken.split("Bearer ")[1];
+  const attributes = parseAttributes(getCookie("SpytnyUserAttributes"));
+  const username = document.getElementById("firstNameUser");
+  username.value = attributes["username"];
+  const userfamilyname = document.getElementById("lastNameUser");
+  userfamilyname.value = attributes["familyname"];
+  const emailElement = document.getElementById("useremail");
+  const emailadress = document.getElementById("emailadressUser");
+  emailElement.textContent = attributes["email"];
+  emailadress.value = attributes["email"];
+
+  postEditUserProfile = function (forms, successCallback, errorCallback) {
+    forms.each(function () {
+      var form = $(this);
+      form.on("submit", function (event) {
+        const firstNameUser = $("#firstNameUser").val();
+        const lastNameUser = $("#lastNameUser").val();
+        const emailadressUser = $("#emailadressUser").val();
+
+        const datatosend = {
+          AccessToken: accessToken,
+          UserAttributes: [
+            {
+              Name: "name",
+              Value: firstNameUser,
+            },
+            {
+              Name: "family_name",
+              Value: lastNameUser,
+            },
+            // {
+            //   Name: "email",
+            //   Value: emailadressUser,
+            // },
+          ],
+        };
+
+        const url = "https://cognito-idp.us-east-1.amazonaws.com/";
+
+        $.ajax({
+          type: "POST",
+          url: url,
+          headers: {
+            "Content-Type": "application/x-amz-json-1.1",
+            "x-amz-target":
+              "AWSCognitoIdentityProviderService.UpdateUserAttributes",
+            Authorization: smartToken,
+          },
+          cors: true,
+          beforeSend: function () {
+            $("#waitingdots").show();
+          },
+          complete: function () {
+            $("#waitingdots").hide();
+          },
+          data: JSON.stringify(datatosend),
+          dataType: "json",
+          success: function (resultData) {
+            if (typeof successCallback === "function") {
+              result = successCallback(resultData);
+              if (!result) {
+                form.show();
+                displayMessage(
+                  "Error",
+                  "Oops. Coś poszło nie tak, spróbuj ponownie."
+                );
+                console.log(e);
+                return;
+              }
+            }
+            form.show();
+            setCookie(
+              "SpytnyUserAttributes",
+              "username:" +
+                firstNameUser +
+                "|familyname:" +
+                lastNameUser +
+                "|email:" +
+                emailadressUser,
+              720000
+            );
+            displayMessage("Success", "Twoje dane zostały zmienione");
+            const welcomeMessage = document.getElementById("welcomeMessage");
+            if (welcomeMessage) {
+              welcomeMessage.textContent =
+                "Witaj, " + firstNameUser + " " + lastNameUser + "!";
+            } else {
+              console.log(
+                "Element 'welcomeMessage' nie został znaleziony. Pomijam ustawienie powitania."
+              );
+            }
+          },
+          error: function (e) {
+            if (typeof errorCallback === "function") {
+              errorCallback(e);
+            }
+            form.show();
+            displayMessage(
+              "Error",
+              "Oops. Coś poszło nie tak, spróbuj ponownie."
+            );
+            console.log(e);
+          },
+        });
+        event.preventDefault();
+        return false;
+      });
+    });
+  };
+
+  postChangePassword = function (forms, successCallback, errorCallback) {
+    forms.each(function () {
+      var form = $(this);
+      form.on("submit", function (event) {
+        var action =
+          "https://hook.eu1.make.com/2laahxeoqfuo7nmf2gh1yyuatq92jiai";
+        var inputdata = form.serializeArray();
+
+        var data = {
+          "Current-Password": inputdata[0].value,
+          "New-Password": inputdata[1].value,
+          AccessToken: accessToken,
+          "User-Email": $("#useremail").text(),
+        };
+
+        $.ajax({
+          type: "POST",
+          url: action,
+          cors: true,
+          beforeSend: function () {
+            $("#waitingdots").show();
+          },
+          complete: function () {
+            $("#waitingdots").hide();
+          },
+          contentType: "application/json",
+          dataType: "json",
+          data: JSON.stringify(data),
+          success: function (resultData) {
+            if (typeof successCallback === "function") {
+              result = successCallback(resultData);
+              if (!result) {
+                form.show();
+                displayMessage(
+                  "Error",
+                  "Oops. Coś poszło nie tak, spróbuj ponownie."
+                );
+                console.log(e);
+                return;
+              }
+            }
+            form.show();
+            displayMessage("Success", "Twoje hasło zostało zmienione.");
+          },
+          error: function (jqXHR, exception) {
+            console.log(jqXHR);
+            console.log(exception);
+            var msg = "";
+            if (jqXHR.status === 0) {
+              msg = "Not connect.\n Verify Network.";
+            } else if (jqXHR.status == 403) {
+              msg = "Użytkownik nie ma uprawnień do tworzenia organizacji.";
+            } else if (jqXHR.status == 400) {
+              msg = "Twoje dotychczasowe hasło jest inne. Spróbuj ponownie.";
+            } else if (jqXHR.status == 500) {
+              msg = "Internal Server Error [500].";
+            } else if (exception === "parsererror") {
+              msg = "Requested JSON parse failed.";
+            } else if (exception === "timeout") {
+              msg = "Time out error.";
+            } else if (exception === "abort") {
+              msg = "Ajax request aborted.";
+            } else {
+              msg = "" + jqXHR.responseJSON.message;
+            }
+            form.show();
+            displayMessage("Error", msg);
+            return;
+          },
+        });
+        event.preventDefault();
+        return false;
+      });
+    });
+  };
+
+  function getCookieNameByValue(searchValue) {
+    // Get all cookies as a single string and split it into individual cookies
+    const cookies = document.cookie.split("; ");
+
+    // Iterate through each cookie string
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const [name, value] = cookie.split("="); // Split each cookie into name and value
+
+      // Decode the cookie value and compare it to the searchValue
+      if (decodeURIComponent(value) === searchValue) {
+        return name; // Return the cookie name if the values match
+      }
+    }
+
+    return null; // Return null if no matching value is found
+  }
+
+  var orgToken = getCookie("sprytnyToken");
+  var DomainName = getCookie("sprytnyDomainName");
+  var ClientID = getCookieNameByValue(orgToken);
+  var InvokeURL = getCookie("sprytnyInvokeURL");
+  var OrganizationName = getCookie("OrganizationName");
+  const OrganizationBread0 = document.getElementById("OrganizationBread0");
+  OrganizationBread0.textContent = OrganizationName;
+  OrganizationBread0.setAttribute(
+    "href",
+    "https://" +
+      DomainName +
+      "/app/tenants/organization?name=" +
+      OrganizationName +
+      "&clientId=" +
+      ClientID
+  );
+
+  const NewpriceListIdBread = document.getElementById("NewpriceListIdBread");
+  NewpriceListIdBread.setAttribute("href", "" + window.location.href);
+  var formIdCreatePricing = "#wf-form-NewPricingList";
+
+  function getWholesalersSh() {
+    let url = new URL(InvokeURL + "wholesalers" + "?enabled=true&perPage=1000");
+    let request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.setRequestHeader("Authorization", orgToken);
+    request.setRequestHeader("Requested-By", "webflow-3-4");
+    request.onload = function () {
+      var data = JSON.parse(this.response);
+      var toParse = data.items;
+      if (request.status >= 200 && request.status < 400) {
+        console.log(Object.keys(toParse).length);
+        const wholesalerContainer =
+          document.getElementById("WholesalerSelector");
+        toParse.forEach((wholesaler) => {
+          if (wholesaler.enabled) {
+            var opt = document.createElement("option");
+            opt.value = wholesaler.wholesalerKey;
+            opt.innerHTML = wholesaler.name;
+            wholesalerContainer.appendChild(opt);
+          }
+        });
+        if (request.status == 401) {
+          console.log("Unauthorized");
+        }
+      }
+    };
+    request.send();
+  }
+
+  function getShops() {
+    let url = new URL(InvokeURL + "shops?perPage=1000");
+    let request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.setRequestHeader("Authorization", orgToken);
+    request.setRequestHeader("Requested-By", "webflow-3-4");
+    request.onload = function () {
+      var data = JSON.parse(this.response);
+      var toParse = data.items;
+
+      if (request.status >= 200 && request.status < 400) {
+        const shopKeysContainer = document.getElementById("shopKeys");
+        toParse.forEach((shop) => {
+          var opt = document.createElement("option");
+          opt.value = shop.shopKey;
+          opt.innerHTML = shop.name;
+          opt.selected = true;
+          shopKeysContainer.appendChild(opt);
+        });
+        if (request.status == 401) {
+          console.log("Unauthorized");
+        }
+        $("option").mousedown(function (e) {
+          e.preventDefault();
+          var originalScrollTop = $(this).parent().scrollTop();
+          console.log(originalScrollTop);
+          $(this).prop("selected", $(this).prop("selected") ? false : true);
+          var self = this;
+          $(this).parent().focus();
+          setTimeout(function () {
+            $(self).parent().scrollTop(originalScrollTop);
+          }, 0);
+
+          return false;
+        });
+      }
+    };
+    request.send();
+  }
+
+  makeWebflowFormAjax = function (forms, successCallback, errorCallback) {
+    forms.each(function () {
+      var form = $(this);
+      form.on("submit", async function (event) {
+        // Dodano async tutaj
+        event.preventDefault();
+
+        // Hide previous error message, if any
+        $("#Create-Pricelist-Fail").stop(true, true).hide();
+
+        var wholesalerKey = $("#WholesalerSelector").val();
+        if (!wholesalerKey) {
+          displayMessage(
+            "Error",
+            "Nie wybrano dostawcy. Proszę wybrać dostawcę z listy."
+          );
+          return false;
+        }
+
+        const formData = new FormData();
+        var uploadEndpoint = InvokeURL + "price-lists";
+
+        // Dodaj plik
+        const uploadedFile = document.getElementById("csv-file").files[0];
+        formData.append("file", uploadedFile, "input.csv");
+
+        // Przygotuj dane JSON
+        const jsonData = {
+          wholesalerKey: wholesalerKey,
+          shopKeys: $("#shopKeys").val(),
+          startDate: $("#startDate").val() + "T00:00:01.00Z",
+          endDate: $("#endDate").val() + "T23:59:59.00Z",
+        };
+
+        // Dodaj dane JSON jako osobną część formularza bez filename
+        formData.append(
+          "json",
+          new Blob([JSON.stringify(jsonData)], {
+            type: "application/json",
+          })
+        );
+
+        try {
+          const response = await axios.post(uploadEndpoint, formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              Authorization: orgToken,
+              "Requested-By": "webflow-3-4",
+            },
+          });
+
+          // success
+          displayMessage("Success", "Cennik został dodany.");
+          var pricelistUrl =
+            "https://" +
+            DomainName +
+            "/app/pricelists/pricelist?uuid=" +
+            response.data.uuid; // Poprawiono uzyskanie uuid
+          window.setTimeout(function () {
+            window.location.href = pricelistUrl;
+          }, 3000);
+        } catch (error) {
+          // error
+          displayMessage("Error", error.message); // Poprawiono wyświetlanie błędów
+          $("#waitingdots").hide();
+        }
+      });
+    });
+  };
+
+  makeWebflowFormAjax($(formIdCreatePricing));
+  getShops();
+  getWholesalersSh();
+  postChangePassword($("#wf-form-Form-Change-Password"));
+  postEditUserProfile($("#wf-form-editProfile"));
+});
