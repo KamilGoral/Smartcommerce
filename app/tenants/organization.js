c465ee25073326cc1afb7b796a90cfa8b0ae4ae7
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-11-03 11:19:15 +0100

    pricats

--- app/tenants/organization.js
+++ app/tenants/organization.js
@@ -2419,7 +2419,7 @@ docReady(function () {
             
                   // Renderuje inne dane dla pojedynczego sklepu
                   if (data.length === 1) {
-                    return `<span class="${statusClass}">${shopDetails}</span>`;
+                    return `<span class="${statusClass}" data-tippy-content="${shopDetails}">${data[0].status}</span>`;
                   }
             
                   // Liczba sklepów i detale w tooltipie dla wielu sklepów
