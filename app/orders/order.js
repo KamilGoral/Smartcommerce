42d786631c1cf444c2a1c6c4c0a65aca77deb4b9
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-11-03 12:52:27 +0100

    order

--- app/orders/order.js
+++ app/orders/order.js
@@ -999,11 +999,11 @@ docReady(function () {
       } else if (selectedWholesalerKey == "unassigned") {
         selectHTML = '<select style="width: 120px;" class="wholesalerSelect">';
         selectHTML += `<option value="unassigned" selected style="font-weight: bold">Nieprzydzielony / Pomiń</option>`;
-        selectHTML += `<option value="cancel" selected style="font-weight: bold">Anuluj mój wybór</option>`;
+        selectHTML += `<option value="remove" selected style="font-weight: bold">Anuluj mój wybór</option>`;
       } else {
         selectHTML = '<select style="width: 120px;" class="wholesalerSelect">';
         selectHTML += `<option value="unassigned" style="font-weight: bold">Nieprzydzielony / Pomiń</option>`;
-        selectHTML += `<option value="cancel" selected style="font-weight: bold">Anuluj mój wybór</option>`;
+        selectHTML += `<option value="remove" selected style="font-weight: bold">Anuluj mój wybór</option>`;
       }
 
       // Sortowanie dostawców z JSON na podstawie klucza 'netPrice', jeśli jsonData nie jest równy null
@@ -1034,7 +1034,7 @@ docReady(function () {
         });
       } else {
         selectHTML += `<option value="unassigned" selected style="font-weight: bold">Nieprzydzielony  / Pomiń</option>`;
-        selectHTML += `<option value="cancel" selected style="font-weight: bold">Anuluj mój wybór</option>`;
+        selectHTML += `<option value="remove" selected style="font-weight: bold">Anuluj mój wybór</option>`;
       }
 
       // Dodawanie pozostałych dostawców z sessionStorage do listy wyboru
@@ -3442,7 +3442,7 @@ docReady(function () {
           // Emulate changes for user
           $("#waitingdots").show(1).delay(150).hide(1);
           checkChangesPayload();
-        } else if (newValue === "cancel") {
+        } else if (newValue === "remove") {
           // Enable the product by setting active to true and assign wholesalerKey if needed
           var activeProduct = {
             op: "replace",
