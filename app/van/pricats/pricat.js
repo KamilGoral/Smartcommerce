5c71abaae436c1e1b40ef8ae5d295435ca7486a4
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-11-03 10:59:00 +0100

    pricats

--- app/van/pricats/pricat.js
+++ app/van/pricats/pricat.js
@@ -809,6 +809,29 @@ docReady(function () {
       });
     });
   };
+  function LoadTippy() {
+    $.getScript(
+      "https://unpkg.com/popper.js@1",
+      function (data, textStatus, jqxhr) {
+        $.getScript(
+          "https://unpkg.com/tippy.js@4",
+          function (data, textStatus, jqxhr) {
+            tippy(".tippy", {
+              // Add the class tippy to your element
+              theme: "light", // Dark or Light
+              animation: "scale", // Options, shift-away, shift-toward, scale, persepctive
+              duration: 250, // Duration of the Animation
+              arrow: true, // Add arrow to the tooltip
+              arrowType: "round", // Sharp, round or empty for none
+              delay: [0, 50], // Trigger delay in & out
+              maxWidth: 240, // Optional, max width settings
+            });
+          }
+        );
+      }
+    );
+  }
+
 
   makeWebflowFormAjaxDeletePriceList($(formIdDeletePriceList));
   makeWebflowFormAjaxEditPriceList($(formIdEditPriceList));
@@ -831,5 +854,6 @@ docReady(function () {
       $.fn.dataTable.tables({ visible: true, api: true }).columns.adjust();
       console.log("Adjusting");
     }, 4000);
+    LoadTippy();
   });
 });
