bd0e665ada440f2b50742da37aa7b39aa842dc23
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-11-06 06:56:46 +0100

    order

--- app/van/pricats/pricat.js
+++ app/van/pricats/pricat.js
@@ -664,44 +664,32 @@ docReady(function () {
     forms.each(function () {
       var form = $(this);
       form.on("submit", function (event) {
-        var container = form.parent();
         var action = `${InvokeURL}van/pricats/${priceListId}`;
         var method = "PATCH";
-        var data = [
-          {
+
+        // Pobierz wartości startDate i endDate
+        const startDate = $("#startDate").val();
+        const endDate = $("#endDate").val();
+
+        // Ustal, które pola mogą być edytowane
+        const editPermissions = isEditable(startDate, endDate, isFtp);
+
+        // Budowanie danych do wysłania tylko dla edytowalnych pól
+        var data = [];
+        if (editPermissions.canEditStartDate) {
+          data.push({
             op: "replace",
             path: "/startDate",
-            value: $("#startDate").val() + "T00:00:01.00Z",
-          },
-          {
+            value: startDate + "T00:00:01.00Z",
+          });
+        }
+        if (editPermissions.canEditEndDate) {
+          data.push({
             op: "replace",
             path: "/endDate",
-            value: $("#endDate").val() + "T23:59:59.00Z",
-          },
-        ];
-
-        // function symmetricDifference(a1, a2) {
-        //   var result = [];
-        //   for (var i = 0; i < a1.length; i++) {
-        //     if (a2.indexOf(a1[i]) === -1) {
-        //       data.push({
-        //         op: "remove",
-        //         path: "/shopKeys/" + a1[i],
-        //       });
-        //     }
-        //   }
-        //   for (i = 0; i < a2.length; i++) {
-        //     if (a1.indexOf(a2[i]) === -1) {
-        //       data.push({
-        //         op: "add",
-        //         path: "/shopKeys/-",
-        //         value: a2[i],
-        //       });
-        //     }
-        //   }
-        // }
-
-        // symmetricDifference(shopKeysStart, $("#shopKeys").val());
+            value: endDate + "T23:59:59.00Z",
+          });
+        }
 
         $.ajax({
           type: method,
@@ -726,14 +714,13 @@ docReady(function () {
             console.log(resultData);
 
             if (typeof successCallback === "function") {
-              result = successCallback(resultData);
+              const result = successCallback(resultData);
               if (!result) {
                 form.show();
                 displayMessage(
                   "Error",
                   "Oops. Coś poszło nie tak, spróbuj ponownie."
                 );
-                console.log(e);
                 return;
               }
             }
