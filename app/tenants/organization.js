c5593978873658633a24acdf54056c6efb0b9573
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2024-12-14 07:51:57 +0100

    fix

--- app/tenants/organization.js
+++ app/tenants/organization.js
@@ -1546,7 +1546,7 @@ docReady(function () {
                 if (data !== null) {
                   // Sprawdzenie, czy vanMember jest true
                   if (row.vanMember) {
-                    return `${data} <span class="positive" style="margin-left: 8px;">SmartVan</span>`;
+                    return `${data} <span class="positive newtippy" data-tippy-content="Aktywuj dostawcę, a integracja rozpocznie się automatycznie" style="margin-left: 8px;">SmartVan</span>`;
                   }
                   return data;
                 }
