cc757f21c0021b2da17cf933df5c0061503bfb3c
KamilGoral <kamil.goral@smartcommerce.net>
2024-12-17 20:36:49 +0100

    dybas

--- app/offers/offer.js
+++ app/offers/offer.js
@@ -999,7 +999,7 @@ docReady(function () {
 
   var table = $("#table_id").DataTable({
     pagingType: "full_numbers",
-    lengthMenu: [25, 50, 100, 500, 1000],
+    lengthMenu: [25, 50, 100, 5000],
     order: [],
     dom: '<"top"fB>rt<"bottom"lip>',
     buttons: [
