dcbd6e1718e4dca55e7917424f4af18629846417
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-02-09 12:52:09 +0100

    search wh

--- app/shops/shop.js
+++ app/shops/shop.js
@@ -1219,9 +1219,9 @@ docReady(function () {
         case "Tak":
           return 2;
         case "Dodaj":
-          return 4;
-        case "Brak":
           return 3;
+        case "Brak":
+          return 4;
         default:
           return 5;
       }
