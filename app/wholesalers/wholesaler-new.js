617eda93d430e703d6c7e79a302f364ccb1ac54a
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-03-21 11:39:33 +0100

    wh new

--- app/wholesalers/wholesaler-new.js
+++ app/wholesalers/wholesaler-new.js
@@ -472,7 +472,7 @@ docReady(function () {
         } else {
           LastStatusMessage.textContent = "Dostawca gotowy do integracji.";
           $("#ehurtStart").removeClass("hide");
-          $("#ehurtStart").removeClass("hide");
+          $("#ehurtBoxDelete").hide();
         }
 
         $("#Wholesaler-profile-Selector-box").hide();
