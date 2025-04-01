acfeb13c8da1ff1bff30871b34610631462089a9
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-04-01 06:59:08 +0200

    wh new

--- app/orders/order-v2.js
+++ app/orders/order-v2.js
@@ -904,7 +904,7 @@ docReady(function () {
                   pdf: '<img src="https://uploads-ssl.webflow.com/6041108bece36760b4e14016/61fd38da3517f633d69e2d58_pdf-FILE.svg" loading="lazy" fileformat="application/pdf" class="filedownloadicon">',
                   xls: '<img src="https://uploads-ssl.webflow.com/6041108bece36760b4e14016/64f899b627cb527b193815cd_TemaSimple.svg" loading="lazy" fileformat="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" class="filedownloadicon">',
                   email:
-                    '<img src="https://cdn.prod.website-files.com/6041108bece36760b4e14016/672d9ae6d7cd2056fac337b6_send.png" class="sendemail" style="width: 24px; height: 24px; cursor: pointer;" />',
+                    '<div class="badgenew" bis_skin_checked="1"><img src="https://cdn.prod.website-files.com/6041108bece36760b4e14016/672d9ae6d7cd2056fac337b6_send.png" class="sendemail" style="width: 24px; height: 24px; cursor: pointer;" /><div bis_skin_checked="1">Nowość</div></div>',
                 };
 
                 // Default content (checkbox and "Realizuj" button)
