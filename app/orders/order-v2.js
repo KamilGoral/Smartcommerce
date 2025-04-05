047923c93f395c44666fe025d4a3220d5b1a5daf
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-04-05 11:28:58 +0200

    wh new

--- app/orders/order-v2.js
+++ app/orders/order-v2.js
@@ -763,14 +763,13 @@ docReady(function () {
                   var toGo = (row.logisticMinimum - row.netValue).toFixed(2);
                   if (toGo > 0) {
                     return `
-                              <div style="display: flex; justify-content: flex-end; align-items: center; gap: 4px;">
-                                  <span>${data}zł</span>
-                                  <span style="color: #8E1212; display: flex; align-items: center;"
-                                        data-tippy-content="Brakuje ${toGo}zł do minimum logistycznego">
+                              <div style="display: flex; justify-content: flex-end; align-items: center; gap: 4px;" data-tippy-content="Brakuje ${toGo}zł do minimum logistycznego>
+                                  <span style="color: #8E1212; display: flex; align-items: center;">
                                       <img src="https://cdn.prod.website-files.com/6041108bece36760b4e14016/67e7b1c29157ff0d17d559a4_tabler_alert-triangle.svg" 
                                            alt="Ostrzeżenie" 
                                            style="width: 16px; height: 16px;">
                                   </span>
+                                  <span>${data}zł</span>
                               </div>
                           `;
                   }
