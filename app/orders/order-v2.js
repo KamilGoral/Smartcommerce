832af8b85dc509d3a281728fbd662704f8135472
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-04-12 11:48:43 +0200

    wh new

--- app/orders/order-v2.js
+++ app/orders/order-v2.js
@@ -3033,7 +3033,7 @@ docReady(function () {
             var patchData = [];
 
             // Pobierz email z inputa i formaty z <select>
-            var email = $("#smtpEmail").val().trim();
+            var email = $("#smtpEmail").val();
             var formats = $("#formats").val();
 
             // Dodaj email, jeśli istnieje
