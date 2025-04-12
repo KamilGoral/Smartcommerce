7739f751a192300b1547e824c5449d19f1091bc0
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-04-12 19:44:19 +0200

    wh new

--- app/orders/order-v2.js
+++ app/orders/order-v2.js
@@ -933,7 +933,7 @@ docReady(function () {
                     '<label class="mylabel" for="' +
                     data +
                     '" style="margin: 0;"></label>' +
-                    '<img src="https://cdn.prod.website-files.com/6041108bece36760b4e14016/67faa4b0c7b0174bfdb32512_advanceButton.svg" alt="Realizuj" class="buttonoutline editme w-button" style="height: 24px; width: auto; cursor: pointer; margin: 0;">' +
+                    '<img src="https://cdn.prod.website-files.com/6041108bece36760b4e14016/67faa4b0c7b0174bfdb32512_advanceButton.svg" alt="Realizuj" style="height: 24px; width: auto; cursor: pointer; margin: 0;">' +
                     "</div>";
                 }
 
