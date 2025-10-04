3e89acc062f399d52a13ef05acb0207a61f8fe8c
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-10-04 06:49:08 +0200

    shop offer view

--- app/orders/order.js
+++ app/orders/order.js
@@ -4777,33 +4777,12 @@ ${offerTimestampLine}
       if (now - lastOfferFetchTimestamp >= MIN_FETCH_INTERVAL_MS) {
         lastOfferFetchTimestamp = now;
         $.get(InvokeURL + "shops/" + shopKey + "/offer" + QStr, function (res) {
-          // Ustawienie daty oferty
-          if (res.offerDate) {
-            const formattedDate = new Date(res.offerDate).toLocaleString(
-              "pl-PL"
-            );
-            $("#offerDate").text("Data oferty: " + formattedDate);
-            $("#offerDate2").text("Data oferty: " + formattedDate);
-          } else {
-            $("#offerDate").text("Data oferty: brak danych");
-            $("#offerDate2").text("Data oferty: brak danych");
-          }
-
-          if (isToday(res.offerDate)) {
-            if (!offerStatusLoaded) {
-              offerStatusLoaded = true;
-              getOfferStatus();
-            }
-
+          if (!offerStatusLoaded) {
+            offerStatusLoaded = true;
+            getOfferStatus();
             $("#offerCondition").show();
             $("#offerTag").show();
             $("#seeRightPanel").show();
-            $("#offerDate2").show();
-          } else {
-            $("#offerCondition").hide();
-            $("#offerTag").hide();
-            $(".seeRightPanel").hide();
-            $("#offerDate2").show();
           }
 
           callback({
