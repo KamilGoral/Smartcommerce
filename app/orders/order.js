43357c1473bbe3cb2a652278832ee1ccdb1a2075
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2025-11-27 07:57:03 +0100

    moja cena podsweitlona

--- app/orders/order.js
+++ app/orders/order.js
@@ -2499,7 +2499,7 @@ whenReadyAndDataTables(function () {
               },
             },
             {
-              orderable: true,
+              orderable: false,
               orderData: [8, 1],
               data: null,
               render: function (data) {
