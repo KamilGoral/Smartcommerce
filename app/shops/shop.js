e7bb6baf1d16ce86579ef03cde883267bf835258
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2026-01-20 08:36:47 +0100

    deliveries

--- app/shops/shop.js
+++ app/shops/shop.js
@@ -934,7 +934,7 @@ whenReadyAndDataTables(function () {
         var sort = "" + whichColumns + direction;
 
         $.get(
-          InvokeURL + "/van/transactions?type=RECADV",
+          InvokeURL + "/van/transactions",
           {
             sort: sort,
             perPage: data.length,
