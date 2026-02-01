f8696e0db877765e2310bbb93b3e0959a691e755
KamilGoral <106865621+KamilGoral@users.noreply.github.com>
2026-02-01 06:32:49 +0100

    delivery

--- app/shops/shop.js
+++ app/shops/shop.js
@@ -5381,7 +5381,7 @@ ${offerTimestampLine}
   getWholesalers();
   getShop();
   getOrders();
-  // getDeliveries();
+  getDeliveries();
 
   $('div[role="tablist"], div[role="tab"], div[role="tabpanel"]').click(
     function () {
